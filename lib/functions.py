8933706be5def944021f22198960b56a84748a8b
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-14 16:03:44 +0300

    passed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
-
+    print("Hello, programmer!")   
 def greet(name):
-    pass
+    print("Hello, " + name + "!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, " + name + "!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+# Example usage
+greet_programmer()
+greet("Alice")
+greet_with_default()
+greet_with_default("Bob")
+result = add(5, 7)
+print("Result of addition:", result)
+halved_value = halve(20)
+print("Half of 20:", halved_value)
+
+
